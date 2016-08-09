e9385b337aa307b71c2b160befccfe647c718d4d
Kim Joo Young <553th@naver.com>
2016-08-09 18:00:57 +0900

    Initial commit

---
+++ app/src/main/java/com/tacademy/miniproject/request/FriendListRequest.java
@@ -0,0 +1,55 @@
+package com.tacademy.miniproject.request;
+
+import android.content.Context;
+
+import com.google.gson.reflect.TypeToken;
+import com.tacademy.miniproject.data.NetworkResult;
+import com.tacademy.miniproject.data.User;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import okhttp3.HttpUrl;
+import okhttp3.Request;
+
+/**
+ * Created by Tacademy on 2016-08-09.
+ */
+public class FriendListRequest extends AbstractRequest<NetworkResult<List<User>>> {
+    Request request;
+    public FriendListRequest(Context context) {
+        HttpUrl url = getBaseUrlBuilder()
+                .addPathSegment("friendlist")
+                .build();
+        request = new Request.Builder()
+                .url(url)
+                .tag(context)
+                .build();
+    }
+
+    @Override
+    public Request getRequest() {
+        return request;
+    }
+
+//    @Override
+//    protected NetworkResult<List<User>> parse(ResponseBody body) throws IOException {
+//        String text = body.string();
+//        Gson gson = new Gson();
+//        NetworkResultTemp temp = gson.fromJson(text, NetworkResultTemp.class);
+//        if (temp.getCode() == 1) {
+//            Type type = new TypeToken<NetworkResult<List<User>>>(){}.getType();
+//            NetworkResult<List<User>> result = gson.fromJson(text, type);
+//            return result;
+//        } else {
+//            Type type = new TypeToken<NetworkResult<String>>(){}.getType();
+//            NetworkResult<String> result = gson.fromJson(text, type);
+//            throw new IOException(result.getResult());
+//        }
+//    }
+
+    @Override
+    protected Type getType() {
+        return new TypeToken<NetworkResult<List<User>>>(){}.getType();
+    }
+}
---
+++ app/src/main/java/com/tacademy/miniproject/request/SignUpRequest.java
@@ -0,0 +1,63 @@
+package com.tacademy.miniproject.request;
+
+import android.content.Context;
+
+import com.google.gson.reflect.TypeToken;
+import com.tacademy.miniproject.data.NetworkResult;
+import com.tacademy.miniproject.data.User;
+
+import java.lang.reflect.Type;
+
+import okhttp3.FormBody;
+import okhttp3.HttpUrl;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+/**
+ * Created by Tacademy on 2016-08-09.
+ */
+public class SignUpRequest extends AbstractRequest<NetworkResult<User>> {
+    Request request;
+    public SignUpRequest(Context context, String username, String password,
+                         String email, String regId) {
+        HttpUrl.Builder builder = getBaseUrlBuilder();
+        builder.addPathSegment("signup");
+
+        RequestBody body = new FormBody.Builder()
+                .add("username",username)
+                .add("password",password)
+                .add("email", email)
+                .add("registrationId", regId)
+                .build();
+
+        request = new Request.Builder()
+                .url(builder.build())
+                .post(body)
+                .tag(context)
+                .build();
+    }
+    @Override
+    public Request getRequest() {
+        return request;
+    }
+
+//    @Override
+//    protected NetworkResult<User> parse(ResponseBody body) throws IOException {
+//        String text = body.string();
+//        Gson gson = new Gson();
+//        NetworkResultTemp temp = gson.fromJson(text, NetworkResultTemp.class);
+//        if (temp.getCode() == 1) {
+//            Type type = new TypeToken<NetworkResult<User>>(){}.getType();
+//            NetworkResult<User> result = gson.fromJson(text, type);
+//            return result;
+//        } else {
+//            Type type = new TypeToken<NetworkResult<String>>(){}.getType();
+//            NetworkResult<String> result = gson.fromJson(text, type);
+//            throw new IOException(result.getResult());
+//        }
+//    }
+    @Override
+    protected Type getType() {
+    return new TypeToken<NetworkResult<User>>(){}.getType();
+}
+}
